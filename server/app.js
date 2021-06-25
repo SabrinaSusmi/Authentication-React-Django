c088095e700953592bdf868b3c6be13f0b8927c9
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-25 22:04:20 +0600

    user route added

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello");
